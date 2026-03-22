0e8e877420409c32f12039952bbae0342087a63c
Hasan <hjhasan@dbgdetroit.org>
2026-03-21 21:47:36 -0400

    Finish Dog API integration, merge main changes, and fix fragment updates

--- app/src/main/java/com/example/cis436_project3/MainActivity.kt
+++ app/src/main/java/com/example/cis436_project3/MainActivity.kt
@@ -1,27 +1,15 @@
 package com.example.cis436_project3
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.cis436_project3.databinding.ActivityMainBinding
 
 // Main entry point of the app
-// It simply loads the main layout which contains the fragments
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // Load the main layout that contains the two fragments
+        setContentView(R.layout.activity_main)
     }
 }
